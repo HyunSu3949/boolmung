9f610a3693d3b736c32133407e4988571bef9383
hyunsu.An <ahs39490215@gmail.com>
2023-04-05 23:40:16 +0900

    feat: chatModel 작성

---
+++ models/chatModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+  room: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'Room',
+  },
+  user: {
+    type: String,
+    required: true,
+  },
+  chat: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Chat', chatSchema);
