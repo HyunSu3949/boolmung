ce796dae9281329c7a9a6876ead6e84b4f9716ee
hyunsu.An <ahs39490215@gmail.com>
2023-04-07 01:18:14 +0900

    feat: enterRoom, removeRoom 미들웨어 작성

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,5 +1,7 @@
-const Room = require('../models/roomModel');
+const Room = require('../schemas/room');
+const Chat = require('../schemas/chat');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.createRoom = catchAsync(async (req, res, next) => {
   const newRoom = await Room.create({
@@ -28,5 +30,45 @@ exports.getAllRoom = catchAsync(async (req, res, next) => {
     data: {
       data: Rooms,
     },
+  });
+});
+
+exports.enterRoom = catchAsync(async (req, res, next) => {
+  const room = await Room.findById(req.params.id);
+
+  if (!room) {
+    return next(new AppError('채팅방이 존재하지 않습니다.', 404));
+  }
+
+  if (room.password && room.password !== req.query.password) {
+    return next(new AppError('비밀번호가 일지하지 않습니다.', 404));
+  }
+
+  const io = req.app.get('io');
+  const { rooms } = io.of('/chat').adapter;
+
+  if (room.max <= rooms.get(req.params.id)?.size) {
+    return next(new AppError('허용 인원을 초과하였습니다.', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      room,
+    },
+  });
+});
+
+exports.removeRoom = catchAsync(async (req, req, next) => {
+  const room = await Room.findByIdAndDelete(req.params.id);
+  const chat = await Chat.deleteMany({ room: req.params.id });
+
+  if (!room) {
+    return next(new AppError('채팅방이 존재하지 않습니다.', 404));
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 });
