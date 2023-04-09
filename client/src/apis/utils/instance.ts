e13f38af7cc121efa15591646db850814e80ab18
hyunsu.An <ahs39490215@gmail.com>
2023-04-09 10:58:14 +0900

    feat: axios 에러 인터셉트 설정

--- client/src/apis/utils/instance.ts
+++ client/src/apis/utils/instance.ts
@@ -2,7 +2,17 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 
 const baseURL = "http://127.0.0.1:3000"; // 개발 로컬주소
 
-export const axiosInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL,
   withCredentials: true,
 });
+
+axiosInstance.interceptors.response.use(
+  (res: AxiosResponse) => res,
+  (error: AxiosError<any>) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
