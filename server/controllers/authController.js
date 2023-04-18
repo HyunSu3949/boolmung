f35ed27f9e7aae21564610b16b06f20e0fef2d19
hyunsu.An <ahs39490215@gmail.com>
2023-04-18 13:27:16 +0900

    httpOnly 옵션 추가

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -17,7 +17,7 @@ const createSendToken = (user, statusCode, req, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000 //1d
     ),
-    sameSite: "none",
+    httpOnly: true,
     secure: req.secure || req.headers("x-forwarded-proto") === "https",
   });
 
